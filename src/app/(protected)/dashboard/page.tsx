3babcda003cdf8f214ff7604da5b1e1aa7b7d788
kaushiktak19 <kaushiktak19@gmail.com>
2024-12-23 12:58:15 +0530

    building dashboard(continued..)

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,48 @@
 'use client'
+import useProject from '@/hooks/use-project'
 import { useUser } from '@clerk/nextjs'
+import { ExternalLink, Github } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const DashboardPage = () => {
-    const {user} = useUser()
+    const {project} = useProject()
     return (
         <div>
-        { user?.firstName}
+            <div className='flex items-center justify-between flex-wrap gap-y-4'>
+                {/* github link */}
+                <div className='w-fit rounded-md bg-primary px-4 py-3'>
+                    <div className="flex items-center">
+                        <Github className='size-5 text-white'/>
+                        <div className='ml-2'>
+                            <p className='text-sm font-medium text-white'>
+                                This projectis linked to {' '}
+                                <Link href={project?.githubUrl ?? ""} className='inline-flex items-center text-white/80 hover:underline'>
+                                    {project?.githubUrl}
+                                    <ExternalLink className='ml-1 size-4'/> 
+                                </Link>
+                            </p>
+                        </div>
+                    </div> 
+                </div>
+
+                <div className='h-4'></div>
+
+                <div className='flex item-center gap-4'>
+                    TeamMemebers
+                    InviteButton
+                    ArchiveButton
+                </div>
+            </div>
+
+            <div className='mt-4'>
+                <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+                    AskQuestions MeetingCard
+                </div>
+            </div>
+
+            <div className='mt-8'></div>
+            Commit Log
         </div>
     )
 }
