03a75ed77fde9dd360f9afc50526b55bd0186bee
kaushiktak19 <kaushiktak19@gmail.com>
2024-12-24 18:42:57 +0530

    commit log feature + gemini for commit summary on dashboard page

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 import {    createTRPCRouter, protectedProcedure   } from "../trpc"
+import { pollCommits } from "@/lib/github"
 
 export const projectRouter = createTRPCRouter({
     createProject: protectedProcedure.input(
@@ -21,7 +22,8 @@ export const projectRouter = createTRPCRouter({
                 }
             }
         })
-
+        
+         await pollCommits(project.id)
         return project
     }),
     getProjects: protectedProcedure.query(async ({ctx}) => {
@@ -35,5 +37,11 @@ export const projectRouter = createTRPCRouter({
                 deleteAt: null
             }
         })
+    }),
+    getCommits: protectedProcedure.input(z.object({
+        projectId: z.string()
+    })).query(async({ctx, input}) => {
+        pollCommits(input.projectId).then().catch(console.error)
+        return await ctx.db.commit.findMany({where: {projectId: input.projectId}})
     })
 })
