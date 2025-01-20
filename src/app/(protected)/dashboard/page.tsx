4a2c524299c7aa26b6de4879db693b1721cfddc7
kaushiktak19 <kaushiktak19@gmail.com>
2025-01-20 19:46:15 +0530

    dynamic import of invite for 'window'

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -8,8 +8,9 @@ import CommitLog from './commit-log'
 import AskQuestionCard from './ask-question-card'
 import MeetingCard from './meeting-card'
 import ArchiveButton from './archive-button'
-import InviteButton from './invite-button'
+const InviteButton =  dynamic(() => import('./invite-button')) 
 import TeamMembers from './team-members'
+import dynamic from 'next/dynamic'
 
 const DashboardPage = () => {
     const {project} = useProject()
